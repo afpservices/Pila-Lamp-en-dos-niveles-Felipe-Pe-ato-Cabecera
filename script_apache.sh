4ba83ae6d30f7c0f797d130c1c9eed5af4d5540b
afpservices <124204166+afpservices@users.noreply.github.com>
2024-10-31 12:13:16 +0100

    Add files via upload

---
+++ script_apache.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install -y apache2
+sudo apt install mysql-server
+sudo apt install php libapache2-mod-php php-mysql
---
+++ script_mysql.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install -y mysql-server
+
+sudo service mysql start
